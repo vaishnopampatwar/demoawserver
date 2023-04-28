23f45500e6f93adff469f066ed112fda1ed88947
vaishnopampatwar <vaishnavipampatwar77@gmail.com>
2023-04-28 08:28:09 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+app=FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World from Smurfcoders"}
+
+@app.get("/test")
+async def root():
+    return {"message":"tjhuwehud"}
+    
